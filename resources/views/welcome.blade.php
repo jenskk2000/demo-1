eaab00035eeff3d938b7b0e6b247497319458f89
jenskristiankristensen <jenskristiankristensen5672@gmail.com>
2015-09-15 22:37:03 +0200

    test 3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">sdfsdfsdgsdgf</div>
+                <div class="title">test</div>
             </div>
         </div>
     </body>
