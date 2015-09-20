8269aa446d5d90d8a72fdc070b3bad5cc3bcac6c
jenskristiankristensen <jenskristiankristensen5672@gmail.com>
2015-09-20 17:39:59 +0200

    test layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,46 +1,110 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>Test</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
-        <style>
-            html, body {
-                height: 100%;
-                background-color: #2ca02c;
-            }
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
 
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
 
-            .title {
-                font-size: 96px;
-            }
-        </style>
     </head>
     <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">test</div>
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-md-8"> <div class="well">
+                    <img src="http://multimedia.pol.dk/archive/00537/ITALY_CLONED_CHAMPI_537998a.jpg" style="width:50%"/>
+                </div></div>
+            <div class="col-md-4">
+
+                <div class="well">
+
+                    <form action="" method="post">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputFile">File input</label>
+                            <input type="file" id="exampleInputFile">
+                            <p class="help-block">Example block-level help text here.</p>
+                        </div>
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox"> Check me out
+                            </label>
+                        </div>
+                        <button type="submit" class="btn btn-default">Submit</button>
+                    </form>
+
+                </div>
+
+            </div>
+
+       </div>
+
+        <div class="row">
+            <div class="col-md-3">
+                <div class="well">
+                    <button type="button" class="btn btn-danger">Knap 1</button>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="well"><button type="button" class="btn btn-primary">Knap 2</button></div>
+            </div>
+            <div class="col-md-3">
+                <div class="well"><button type="button" class="btn btn-success">Knap 3</button></div>
+            </div>
+            <div class="col-md-3">
+               <div class="well">
+                   <button type="button" class="btn btn-warning">Knap 4</button>
+               </div>
             </div>
         </div>
+
+        <div class="row">
+            <div class="col-md-6">
+                <div class="well" id="log2">test</div>
+            </div>
+            <div class="col-md-6">
+                <div class="well" id="log">test</div>
+            </div>
+        </div>
+
+
+    </div>
+
+
+    <script>
+
+        $( "button" ).on( "click", function( event ) {
+
+            console.log( $(this).text() );
+            $("#log").html($(this).text()+"<br/>");
+
+            $("#log2").html($(this).text()+"<br/>");
+
+        });
+
+
+
+
+
+
+    </script>
+
     </body>
 </html>
